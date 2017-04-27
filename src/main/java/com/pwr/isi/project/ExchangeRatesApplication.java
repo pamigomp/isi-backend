c7d23a26b5c3f2d6246e9628e822a85f27145029
Michal Pietrzak <201011@student.pwr.wroc.pl>
2017-04-27 19:07:42 +0200

    Fix host name.

--- src/main/java/com/pwr/isi/project/ExchangeRatesApplication.java
+++ src/main/java/com/pwr/isi/project/ExchangeRatesApplication.java
@@ -8,7 +8,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class ExchangeRatesApplication {
 
-  public static final String HOST_URL = "https://isi-backend.herokuapp.com/";
+  public static final String HOST_URL = "https://isi-frontend.herokuapp.com/";
 
   public static void main(String[] args) {
     SpringApplication.run(ExchangeRatesApplication.class, args);
